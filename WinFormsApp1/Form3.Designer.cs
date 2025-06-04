7d862a5666a560e00b29358f04900ad9ba5be8a1
simon <simonrebros07@gmail.com>
2025-06-04 21:33:06 +0200

    lvls

--- WinFormsApp1/Form3.Designer.cs
+++ WinFormsApp1/Form3.Designer.cs
@@ -28,73 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            pictureBox4 = new PictureBox();
+            pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackColor = Color.Black;
-            pictureBox1.Location = new Point(360, 151);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(154, 251);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            pictureBox2.BackColor = Color.Black;
-            pictureBox2.Location = new Point(182, 151);
+            pictureBox2.BackColor = Color.Cyan;
+            pictureBox2.Location = new Point(173, 231);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(154, 251);
+            pictureBox2.Size = new Size(171, 112);
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            pictureBox3.BackColor = Color.Black;
-            pictureBox3.Location = new Point(538, 151);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(154, 251);
-            pictureBox3.TabIndex = 2;
-            pictureBox3.TabStop = false;
-            // 
             // button1
             // 
+            button1.BackColor = Color.FromArgb(192, 0, 192);
             button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            button1.Location = new Point(182, 348);
+            button1.ForeColor = Color.Transparent;
+            button1.Location = new Point(182, 244);
             button1.Name = "button1";
-            button1.Size = new Size(154, 54);
+            button1.Size = new Size(154, 87);
             button1.TabIndex = 3;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button1.Text = "Level 1";
+            button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            button2.BackColor = Color.Purple;
             button2.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            button2.Location = new Point(360, 348);
+            button2.ForeColor = Color.FromArgb(224, 224, 224);
+            button2.Location = new Point(360, 244);
             button2.Name = "button2";
-            button2.Size = new Size(154, 54);
+            button2.Size = new Size(154, 87);
             button2.TabIndex = 4;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            button2.Text = "Level 2";
+            button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
+            button3.BackColor = Color.FromArgb(64, 0, 64);
             button3.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            button3.Location = new Point(538, 348);
+            button3.ForeColor = Color.Silver;
+            button3.Location = new Point(538, 244);
             button3.Name = "button3";
-            button3.Size = new Size(154, 54);
+            button3.Size = new Size(154, 87);
             button3.TabIndex = 5;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            button3.Text = "Level 3";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Cyan;
+            pictureBox4.Location = new Point(351, 231);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(171, 112);
+            pictureBox4.TabIndex = 6;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Cyan;
+            pictureBox1.Location = new Point(528, 231);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(172, 112);
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
             // 
             // Form3
             // 
@@ -105,15 +111,15 @@
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
+            Controls.Add(pictureBox4);
             Name = "Form3";
             Text = "Form3";
             Load += Form3_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -125,5 +131,6 @@
         private Button button1;
         private Button button2;
         private Button button3;
+        private PictureBox pictureBox4;
     }
 }
